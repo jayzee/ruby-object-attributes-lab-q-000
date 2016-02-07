22c3728a2e005ec1cbc34cb07b264c9fef244a2d
jayzee <joshzizmor@gmail.com>
2016-02-06 22:46:37 -0500

    update the code based on lab instructions

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(what_is_the_breed)
+    @breed = what_is_the_breed
+  end
+
+  def breed
+    @breed
+  end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name=(what_is_the_persons_name)
+    @name = what_is_the_persons_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(what_is_the_job)
+    @job = what_is_the_job
+  end
+
+  def job
+    @job
+  end
+
+end
